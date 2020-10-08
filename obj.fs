a996a8f7341de5f4b8afcfc9309e7c6c1cb22397
Charles SAYAMATH <charles.sayamath@etu.unilim.fr>
2020-10-08 20:15:59 +0200

    Fresnel fonctionne

--- obj.fs
+++ obj.fs
@@ -9,7 +9,9 @@ varying vec3 N;
 
 uniform vec3 uLightPos;
 uniform vec3 uLightPower;
-uniform vec3 uKd;
+uniform vec3 color;
+uniform float uKd;
+uniform float uKs;
 
 const float M_PI = 3.14159265358;
 
@@ -24,7 +26,7 @@ vec3 lambert( vec3 lightPower, vec3 kD ,vec3 normale,vec3 wi){
 
 float fresnel(vec3 wi,vec3 halfVector, float n){
 
-	float c = dot(wi,halfVector);
+	float c = abs(dot(normalize(wi),halfVector));
 	float g = sqrt(n*n + c * c -1.0);
 	float a = 0.5 * (g-c)*(g-c) / ((g+c)*(g+c));
 	float b = (c*(g+c)-1.0) * (c*(g+c)-1.0) / ( (c*(g-c) +1.0) * (c*(g-c) +1.0));
@@ -49,6 +51,8 @@ float cook_torrance(vec3 normale, vec3 halfVector, vec3 wi, vec3 wo){
 	return min(1.0,min(a,b));
 }
 
+
+
 // =====================================================
 void main(void)
 {
@@ -56,8 +60,13 @@ void main(void)
 	vec3 wo = (- vec3(pos3D));
 	vec3 halfVector =  normalize(wi+wo);
 	//vec3 col = lambert(uLightPower,uKd,N,wi); 
+	float n = (1.0-1.33) / (1.0+1.33);
 	float c = fresnel(wi,halfVector, 1.33);
-	vec3 col =c * vec3(1.0);
+
+	float dot_in = abs(dot(normalize(wi),N));
+	float dot_on = abs(dot(normalize(wo),N));
+
+	vec3 col =(c/(4.0*dot_in*dot_on));
 	
 	gl_FragColor = vec4(col,1.0);
 }
